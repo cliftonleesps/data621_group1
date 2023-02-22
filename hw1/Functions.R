865ae29610a308d4065f67d7cc4e71f0b3e8f9f0
dsimband <65495549+dsimband@users.noreply.github.com>
2023-02-22 09:46:01 -0500

    debugged dataframe filter

--- hw1/Functions.R
+++ hw1/Functions.R
@@ -4,9 +4,9 @@ library(dplyr)
 
 
 
-#' Title
+#' Load the Training Stats File
 #'
-#' @return
+#' @return Dataframe with Stats
 #' @export
 #'
 #' @examples
@@ -20,9 +20,9 @@ loadClassData <- function() {
 
 
 
-#' Title
+#' Load Dataframe with Lahman Stats
 #'
-#' @return
+#' @return Dataframe with clean stats
 #' @export
 #'
 #' @examples
@@ -59,6 +59,9 @@ loadLahmanData <- function() {
   teamDF$TEAM_FIELDING_DP <- teamDF$gFactor * teamDF$DP
 
 
+  # round values
+  teamDF <- teamDF %>% mutate_if(is.numeric, round)
+
   # Pythagorean winning percentage
   #  (runs scored ^ 2) / [(runs scored ^ 2) + (runs allowed ^ 2)]
   teamDF$pythPercent <- (teamDF$R^2) / ((teamDF$R^2) + (teamDF$RA^2))
@@ -76,9 +79,9 @@ loadLahmanData <- function() {
 
 
 
-#' Title
+#' Load Dataframe with Shorter version of Lahman Stats
 #'
-#' @return
+#' @return Dataframe with clean stat
 #' @export
 #'
 #' @examples
@@ -99,55 +102,7 @@ loadLahmanShortData <- function() {
 
 
 
-
-
-loadRangeList <- function(filter_df) {
-
-  df <- select_if(filter_df, is.numeric) %>% drop_na()
-
-  rl <- list()
-
-  for(i in names(df)) {
-    #mi <- min(df[i])
-    #ma <- max(df[i])
-    print(paste0(i, '-', min(df[i])))
-    rl[paste0(i,'_MIN')] <- min(df[i])
-    rl[paste0(i,'_MAX')] <- max(df[i])
-  }
-
-  return (rl)
-
-}
-
-
-
-x <- sapply(filter_df, min, na.rm = TRUE)
-typeof(x)
-x[i]
-
-
-df <- select_if(filter_df, is.numeric)
-min_lst <- round(sapply(df, min, na.rm = TRUE),0)
-max_lst <- round(sapply(df, max, na.rm = TRUE),0)
-
-
-
-
-
-
-rl <- loadRangeList(filter_df)
-rl['TARGET_WINS_MIN']
-min(filter_df['TARGET_WINS'])
-
-
-
-i <- 'TARGET_WINS'
-min(filter_df[i])
-rl[paste0(i,'_MIN')] <- min(filter_df[i])
-rl
-
-
-#' Title
+#' Filter outliers using the min and max from a filter dataframe
 #'
 #' @param filter_df
 #' @param df
@@ -160,79 +115,35 @@ rl
 filterOutliers <- function(filter_df, df, columnNames) {
 
   filter_df <- select_if(filter_df, is.numeric)
+
+  #columnNames <- intersect(columnNames,names(filter_df))
+  #columnNames <- intersect(columnNames, names(df))
+
   min_lst <- round(sapply(filter_df, min, na.rm = TRUE),0)
   max_lst <- round(sapply(filter_df, max, na.rm = TRUE),0)
 
+
+  df$omit <- FALSE
+
   for (i in columnNames) {
-    df <- df[df[i] > min_lst[[i]] & df[i] < max_lst[[i]] ,]
+    tryCatch(
+      #df <- df[df[i] >= min_lst[[i]] & df[i] <= max_lst[[i]] ,]
+      df$omit[df[i] < min_lst[[i]] | df[i] > max_lst[[i]]] <- TRUE
+      ,
+
+      error=function(e) e
+    )
   }
 
+  df <- df %>% dplyr::filter(!omit)
   return (df)
-
 }
 
 
 
 
-min_lst <- sapply(filter_df, min, na.rm = TRUE)
-max_lst <- sapply(filter_df, max, na.rm = TRUE)
 
-df <- droppedDf
-n <- 'TARGET_WINS'
-
-x <- droppedDf
-x <- df[which(df[n] >= min_lst[[n]]),]
-x <- df[which(df[n] <= max_lst[[n]]),]
-
-
-
-
-
-
-
-
-x <- df[which(max_lst[[i]] >= df[i]),]
-
-
-
-
-df %>% filter()
-
-df
-df[df[i] > min_lst[[i]] & df[i] < max_lst[[i]] ,]
-
-
-max(df[i])
-min(df[i])
-
-
-
-
-x <- df[(df[i] > min_lst[[i]]) & (df[i] < max_lst[[i]]) ,]
-
-min_lst[[i]]
-
-rangeList[vMin]
-
-
-# setup
-# filter_df <- loadLahmanShortData()
-# df <- loadClassData()
-# columnNames <- c('TARGET_WINS','TEAM_BATTING_H')
-# df0 <- filterOutliers(filter_df,df,columnNames)
-#
-#
-# g <- rangeList[[vMax]]
-# x <- df[paste0('df$',i) > rangeList[[vMin]],]
-# x <- df[df[i] > rangeList[[vMin]],]
-#
-# x <- df[df[i] > rangeList[[vMin]] & df[i] < rangeList[[vMax]] ,]
-# df[i]
-# x <- df[df$TARGET_WINS > 43,]
-# i <- 'TARGET_WINS'
-
-
-#' Title
+#' Print General Stats
 #'
 #' @param df
 #'
@@ -274,8 +185,36 @@ printStats <- function(df) {
     method = "viewer"
   )
 
-
 }
+
+
+
+
+#' Print Density Plots
+#'
+#' @param df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+  printDensityPlot <- function(df) {
+
+    m_df <- df %>%  melt()
+
+    p1 <- m_df %>% ggplot(aes(x= value)) +
+      geom_density(color='#023020', fill='gray') + facet_wrap(~variable, scales = 'free',  ncol = 4) + theme_bw()
+    print(p1)
+
+    p2 <- m_df %>% ggplot(aes(x = value)) +
+      geom_boxplot(outlier.color = 'red', outlier.shape = 1) +
+      facet_wrap(vars(variable),scales = "free", ncol = 4)
+
+    print(p2)
+
+  }
+
+
 
 
 
