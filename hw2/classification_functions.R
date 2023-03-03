33fa684526f99e0f05af162895dca2fa8584c24f
nolivercuny <nicholas.oliver85@spsmail.cuny.edu>
2023-03-02 19:50:01 -0600

    suggested edits

--- hw2/classification_functions.R
+++ hw2/classification_functions.R
@@ -3,18 +3,17 @@ library(dplyr)
 
 
 classify_predictions <- function(df) {
+    dfSelected <- df %>% select(class, scored.class)
+    true_negative <- dfSelected %>% filter(scored.class == 0 & class == 0)
+    true_positive <- dfSelected %>% filter(scored.class == 1 & class == 1)
 
-    true_negative <- df %>% select(class, scored.class) %>% filter(scored.class == 0 & class == 0)
-    true_positive <- df %>% select(class, scored.class) %>% filter(scored.class == 1 & class == 1)
+    false_negative <- dfSelected %>% filter(scored.class == 0 & class == 1)
+    false_positive <- dfSelected %>% filter(scored.class == 1 & class == 0)
 
-    false_negative <- df %>% select(class, scored.class) %>% filter(scored.class == 0 & class == 1)
-    false_positive <- df %>% select(class, scored.class) %>% filter(scored.class == 1 & class == 0)
-
-    matrix <- list("tn" = nrow(true_negative),
+    list("tn" = nrow(true_negative),
                    "tp" = nrow(true_positive),
                    "fn" = nrow(false_negative),
                    "fp" = nrow(false_positive))
-    return (matrix)
 }
 
 
@@ -22,24 +21,21 @@ classify_predictions <- function(df) {
 ## classifications identified, and returns the accuracy of the predictions.
 test_accuracy <- function(df) {
     m <- classify_predictions(df)
-    accuracy <- (m$tp + m$tn) / (m$tp + m$fp + m$tn + m$fn)
-    return (accuracy)
+    (m$tp + m$tn) / (m$tp + m$fp + m$tn + m$fn)
 }
 
 ## 4. Write a function that takes the data set as a dataframe, with actual and
 ## predicted classifications identified, and returns the classification error rate of the predictions.
 test_classification_error_rate <- function(df) {
     m <- classify_predictions(df)
-    cer <- (m$fp + m$fn) / (m$tp + m$fp + m$tn + m$fn)
-    return (cer)
+    (m$fp + m$fn) / (m$tp + m$fp + m$tn + m$fn)
 }
 
 ## 5. Write a function that takes the data set as a dataframe, with actual and
 ## predicted classifications identified, and returns the precision of the predictions.
 test_precision <- function(df) {
     m <- classify_predictions(df)
-    p <- m$tp / (m$tp + m$fp)
-    return (p)
+    m$tp / (m$tp + m$fp)
 }
 
 ## 6. Write a function that takes the data set as a dataframe, with actual and
@@ -47,16 +43,14 @@ test_precision <- function(df) {
 ## Sensitivity is also known as recall.
 test_sensitivity <- function(df) {
     m <- classify_predictions(df)
-    s <- (m$tp) / (m$tp + m$fn)
-    return (s)
+    (m$tp) / (m$tp + m$fn)
 }
 
 ## 7. Write a function that takes the data set as a dataframe, with actual and predicted
 ## classifications identified, and returns the specificity of the predictions.
 test_specificity <- function(df) {
     m <- classify_predictions(df)
-    s <- (m$tn) / (m$tn + m$fp)
-    return (s)
+    (m$tn) / (m$tn + m$fp)
 }
 
 ## 8. Write a function that takes the data set as a dataframe, with actual and
@@ -65,6 +59,5 @@ test_f1_score <- function(df) {
     precision <- precision(df)
     sensitivity <- sensitivity(df)
 
-    score <- (2 * precision * sensitivity) / (precision + sensitivity)
-    return(score)
+    (2 * precision * sensitivity) / (precision + sensitivity)
 }
